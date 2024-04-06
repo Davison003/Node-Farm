88502860227dfac77b5a3ace2790d7d623b5861f
Davi <daviregonatti@gmail.com>
2024-04-06 02:12:11 -0300

    fix: fixed url names

--- src/index.js
+++ src/index.js
@@ -3,90 +3,65 @@ const http = require("http");
 const url = require("url");
 
 const replaceTemplate = require("./modules/replaceTemplate.js");
-//FILES
-
-//Blocking, synchronous way
-
-// const textIn = fs.readFileSync("./txt/input.txt", "utf8");
-// const textOut = `This is info about the avocado: ${textIn}.\n Created on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log(textIn);
-
-//Non-blocking, asynchronous way
-
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//   });
-// });
-// console.log("Will read file");
-
+const slugify = require("slugify");
 //SERVER
 
 // Reading Files
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const productData = JSON.parse(data);
 
-const tempOverview = fs.readFileSync(
-  `${__dirname}/templates/html/template-overview.html`,
-  "utf-8"
-);
+const tempOverview = fs.readFileSync(`${__dirname}/templates/html/template-overview.html`, "utf-8");
 
-const tempProduct = fs.readFileSync(
-  `${__dirname}/templates/html/template-product.html`,
-  "utf-8"
-);
+const tempProduct = fs.readFileSync(`${__dirname}/templates/html/template-product.html`, "utf-8");
 
-const tempCard = fs.readFileSync(
-  `${__dirname}/templates/html/template-card.html`,
-  "utf-8"
-);
+const tempCard = fs.readFileSync(`${__dirname}/templates/html/template-card.html`, "utf-8");
 
+const slugs = productData.map((el) => slugify(el.productName, { lower: true }));
+
+// Creating the server
 const server = http.createServer(function (req, res) {
-  // console.log(req.headers);
+    // console.log(req.headers);
 
-  const { query, pathname } = url.parse(req.url, true);
+    const { query, pathname } = url.parse(req.url, true);
 
-  // overview page
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, {
-      "Content-type": "text/html",
-    });
+    // overview page
+    if (pathname === "/" || pathname === "/overview") {
+        res.writeHead(200, {
+            "Content-type": "text/html",
+        });
 
-    const cardsHtml = productData
-      .map((el) => replaceTemplate(tempCard, el))
-      .join("");
+        const cardsHtml = productData.map((el) => replaceTemplate(tempCard, el)).join("");
 
-    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
-    res.end(output);
+        const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+        res.end(output);
 
-    // product page
-  } else if (pathname === "/product") {
-    res.writeHead(200, {
-      "Content-type": "text/html",
-    });
-    const product = productData[query.id];
-    const output = replaceTemplate(tempProduct, product);
+        // product page
+    } else if (pathname === "/product") {
+        res.writeHead(200, {
+            "Content-type": "text/html",
+        });
+        const product = productData.find((el) => el.slug === slugs[query.id]);
+        const output = replaceTemplate(tempProduct, product);
 
-    res.end(output);
+        res.end(output);
 
-    // api page
-  } else if (pathname === "/api") {
-    res.writeHead(200, {
-      "Content-type": "application/json",
-    });
-    res.end(data);
+        // api page
+    } else if (pathname === "/api") {
+        res.writeHead(200, {
+            "Content-type": "application/json",
+        });
+        res.end(data);
 
-    // not found page
-  } else {
-    res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-header": "hello there",
-    });
-    res.end(`<h1>Page not Found! - ${pathname}</h1>`);
-  }
+        // not found page
+    } else {
+        res.writeHead(404, {
+            "Content-type": "text/html",
+            "my-header": "hello there",
+        });
+        res.end(`<h1>Page not Found! - ${pathname}</h1>`);
+    }
 });
 
 server.listen(8000, "localhost", () => {
-  console.log(`Server listening for requests`);
+    console.log(`Server listening for requests`);
 });
